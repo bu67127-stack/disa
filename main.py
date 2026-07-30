bb1111a7c786dd97f0d47c688d7b59237a09d287
bu67127-stack <bu67127@gmail.com>
2026-07-30 18:01:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import discord
 from discord.ext import commands
 
+
+loadbot = os.getenv("discordbotrun")
+
 intents = discord.Intents.all()
 bot = discord.Bot(command_prefix="$", intents=intents)
 
 @bot.event
 async def on_ready():
     print("Bot ReOnline")
+    await client.change_presence(activity=discord.Game(name="서버 보호중 ($설명서)"))
 
-bot.run()
+bot.command(aliases="설명서")
+async def help()
+
+
+bot.run(loadbot)
